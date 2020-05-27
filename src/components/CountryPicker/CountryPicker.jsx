c881a0993a7f3a53c825a3b4a9645c4a25f68c18
Zeyesa Hinji <hinjizeyesa@gmail.com>
2020-05-27 13:30:51 +0300

    make it better

---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import {NativeSelect, FormControl} from '@material-ui/core';
+
+import styles from './CountryPicker.module.css';
+
+import {fetchCountries} from '../../api';
+
+const CountryPicker = ({ handleCountryChange }) => {
+
+    const [fetchedCountries, setFetchedCounties] = useState([]);
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            setFetchedCounties(await fetchCountries());
+        }
+        fetchApi();
+    }, [setFetchedCounties]);
+    return(
+        <FormControl className={styles.FormControl}>
+            <NativeSelect defaultValue = " " onChange={(e) => handleCountryChange(e.target.value)}>
+                <option value="global">Choose Country</option>
+
+                {fetchedCountries.map((country,i) => <option key={i} value ={country}> {country}</option>)}
+
+            </NativeSelect>
+        </FormControl>
+    )
+}
+
+export default CountryPicker;
